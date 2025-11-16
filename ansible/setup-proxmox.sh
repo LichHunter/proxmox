77e5cc1f73ea242e82d3721004505f460151a516
Alexander <alexander0derevianko@gmail.com>
2025-11-16 13:25:38 +0100

    Fix zsh task, update scripts

--- ansible/setup-proxmox.sh
+++ ansible/setup-proxmox.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env sh
+#! /usr/bin/env nix
+#! nix shell nixpkgs#ansible --command bash
 
-ansible-playbook -i inventory/hosts.yaml playbooks/setup-proxmox-host.yaml
+ansible-playbook -i inventory/hosts.dev.yaml playbooks/setup-proxmox-host.yaml
 
-ansible-playbook -i inventory/hosts.yaml playbooks/configure-lxc.yaml
+ansible-playbook -i inventory/hosts.dev.yaml playbooks/configure-lxc.yaml --extra-vars "@vars/dev.yaml"
