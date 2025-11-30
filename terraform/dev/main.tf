d148814522a759e8bf36c12d0fac286511f174d7
Alexander <alexander0derevianko@gmail.com>
2025-11-30 14:54:17 +0100

    Prometheus configuration

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -395,6 +395,57 @@ resource "proxmox_virtual_environment_container" "nginx_container" {
   }
 }
 
+resource "proxmox_virtual_environment_container" "prometheus_container" {
+  description = "Managed by Terraform"
+
+  node_name    = var.node_name
+  vm_id        = 1243
+  tags         = ["ansible_managed", "prometheus"]
+  unprivileged = true
+
+  cpu {
+    cores = 4
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    datastore_id = var.datastore_id
+    size         = 12
+  }
+
+  initialization {
+    hostname = "prometheus"
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    dns {
+      domain  = "homelab.com"
+      servers = ["192.168.100.1"]
+    }
+  }
+
+  features {
+    nesting = true
+  }
+
+  network_interface {
+    name   = "eth1"
+    bridge = "vmbr1"
+  }
+
+  operating_system {
+    template_file_id = proxmox_virtual_environment_download_file.debian_13_lxc_img.id
+    type             = "debian"
+  }
+}
+
 resource "proxmox_virtual_environment_network_linux_bridge" "vmbr1" {
   node_name = var.node_name
   name      = "vmbr1"
