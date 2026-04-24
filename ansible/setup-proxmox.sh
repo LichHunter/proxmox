8799b9608704ac55b4782bceb4c61826ec91b249
Alexander <alexander0derevianko@gmail.com>
2026-04-24 20:53:44 +0200

    Cleanup old ansible

--- ansible/setup-proxmox.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env nix
-#! nix shell nixpkgs#ansible --command bash
-
-ansible-playbook -i inventory/hosts.dev.yaml playbooks/setup-proxmox-host.yaml
-
-ansible-playbook -i inventory/hosts.dev.yaml playbooks/configure-lxc.yaml --extra-vars "@vars/dev.yaml"
