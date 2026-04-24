8e137f60eda03b5bcbc98f9e107c77784ce44155
Alexander <alexander0derevianko@gmail.com>
2026-04-24 14:50:34 +0200

    Fixed debian image

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,21 +17,10 @@ provider "proxmox" {
 resource "proxmox_virtual_environment_download_file" "debian_12_lxc_img" {
   content_type = "vztmpl"
   datastore_id = "local"
-  node_name    = "proxmox"
+  node_name    = var.node_name
   # https://forum.proxmox.com/threads/solved-automating-with-bpg-proxmox-how-to-find-url-and-checksum-of-lxc-images.140315/
-  url                = "http://download.proxmox.com/images/system/debian-12-standard_12.2-1_amd64.tar.zst"
-  checksum           = "1846c5e64253256832c6f7b8780c5cb241abada3ab0913940b831bf8f7f869220277f5551f0abeb796852e448c178be22bd44eb1af8c0be3d5a13decf943398a"
+  url                = "http://download.proxmox.com/images/system/debian-12-standard_12.12-1_amd64.tar.zst"
+  checksum           = "50c85eaaece677a3ebe01cc909b83872e9da2a22c29ae652838afce71e83222fdf40f6accecd7d52b180e912fc1f85ecdf7b3fc4d3027da4d865e509a9e76597"
   checksum_algorithm = "sha512"
   upload_timeout     = 300
-}
-
-resource "random_password" "ubuntu_container_password" {
-  length           = 16
-  override_special = "_%@"
-  special          = true
-}
-
-resource "tls_private_key" "ubuntu_container_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
 }
